e260e4a21c429a426f65337466724141c7c9699f
sasayu50202 <sasayu50202@gmail.com>
2024-11-20 11:26:39 +0900

    状態のリフトアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
+import { Todo } from "@/types";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
+const TODOS: Todo[] = [
+  {
+    id: 1,
+    text: "foo",
+    isDone: false,
+  },
+  {
+    id: 2,
+    text: "bar",
+    isDone: true,
+  },
+];
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [todos, setTodos] = useState<Todo[]>(TODOS);
+
+  return <Component {...pageProps} todos={todos} setTodos={setTodos} />;
 }
