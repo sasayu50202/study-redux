f9cdc477437ee9c366a5813a94b5cd27cc84e051
sasayu50202 <sasayu50202@gmail.com>
2024-11-20 14:31:39 +0900

    store作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,11 @@
-import { Todo } from "@/types";
+import { store } from "@/state";
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { Provider } from "react-redux";
 
-const TODOS: Todo[] = [
-  {
-    id: 1,
-    text: "foo",
-    isDone: false,
-  },
-  {
-    id: 2,
-    text: "bar",
-    isDone: true,
-  },
-];
 export default function App({ Component, pageProps }: AppProps) {
-  const [todos, setTodos] = useState<Todo[]>(TODOS);
-
-  return <Component {...pageProps} todos={todos} setTodos={setTodos} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
